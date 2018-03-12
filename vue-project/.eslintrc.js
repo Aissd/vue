fd06275b50b2b5245dd0f39dab91d1aaadc7c92e
If <247596814@qq.com>
2018-03-12 20:37:51 +0800

    update

---
+++ vue-project/.eslintrc.js
@@ -0,0 +1,33 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
+    'globals': {
+        
+    },
+    env: {
+        browser: true,
+        'es6': true,
+        'commonjs': true,
+        'node': true
+    },
+    extends: [
+      'plugin:vue/essential', 
+      'eslint:recommended'
+    ],
+    // required to lint *.vue files
+    //   plugins: [
+    //     'html'
+    //   ],
+    // add your custom rules here
+    rules: {
+        // allow async-await
+        'generator-star-spacing': 'off',
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    }
+}
---
+++ vue-project/src/page/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <!-- toggle 折叠列表 
+        1、给列表的每个item值添加一个len属性（item是一个对象）
+        2、隐藏的原理是比对index值，index小于len属性，则隐藏
+        3、
+    -->
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>产品</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(tr, tr_index) in tableList" :key="tr_index">
+                <td @click="viewMore(tr)">
+                    <ul>
+                        <li v-if="td_index < tr.len" v-for="(td, td_index) in tr.part_name" :key="td_index" v-text="td.part_name"></li>
+                        <li v-if="tr.part_name.length > 3 && tr.len == 3">...</li>
+                        <li v-if="tr.part_name.length === 0">空</li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    import indexData from '@/datas/index.json';
+    export default {
+        name: 'index',
+        data() {
+            return {
+                tableList: []
+            };
+        },
+        mounted() {
+            this.pageInitFn();
+        },
+        methods: {
+            pageInitFn() {
+                let data = indexData.data;
+                this.tableList = data.map(item => {
+                    item.len = 3;
+                    return item;
+                });
+            },
+            viewMore(target) {
+                target.len = target.len == 3 ? target.part_name.length : 3;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
