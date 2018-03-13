f5fb31d3e87ba07841dd3c15ebb6b2d4e899fe16
If <247596814@qq.com>
2018-03-13 15:13:03 +0800

    update

--- vue-project/src/page/Tab.vue
+++ vue-project/src/page/Tab.vue
@@ -1,8 +1,12 @@
 <template>
+    <!-- 
+        1、tab页，根据参数切换列表
+        2、由于地址栏有参数，所以刷新也不会影响
+     -->
     <div class="tab">
         <div class="tab-bar">
-            <button type="button" class="btn">已上架</button>
-            <button type="button" class="btn">待下架</button>
+            <button type="button" class="btn btn-default" :class="{ 'btn-primary': tab_status == 1 }" @click="selectTab({ tab_status: 1 })">已上架</button>
+            <button type="button" class="btn btn-default" :class="{ 'btn-primary': tab_status == 2 }" @click="selectTab({ tab_status: 2 })">待下架</button>
         </div>
         <div class="tab-container">
             <table class="table table-bordered">
@@ -13,16 +17,14 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="javascript:;">上架</a>
+                    <tr v-for="(item, index) in tableList" :key="index">
+                        <td v-text="item.part_name"></td>
+                        <td v-if="item.tab_status == 1">
+                            <a href="javascript:;">下架</a>
                             <a href="javascript:;">详情</a>
                         </td>
-                        <td>
-                            <a href="javascript:;">下架</a>
+                        <td v-else>
+                            <a href="javascript:;">上架</a>
                             <a href="javascript:;">编辑</a>
                             <a href="javascript:;">详情</a>
                         </td>
@@ -34,12 +36,13 @@
 </template>
 
 <script>
+    import router from '@/router';
     import tabData from '@/datas/tab.json';
     export default {
         name: 'Tab',
         data() {
             return {
-                status: 1, // 1已上架，2待上架
+                tab_status: null, // 1已上架，2待上架
                 tableList: []
             };
         },
@@ -48,11 +51,20 @@
         },
         methods: {
             pageInitFn() {
-                let data = tabData.data;
-                this.tableList = data.map(item => item.part_name).filter(itm => {
-                    console.log(itm);
+                this.tab_status = this.$route.query.status == undefined ? 1 : this.$route.query.status;
+                this.tableList = tabData.data.filter(item => item.status == this.tab_status);
+            },
+            selectTab(query) {
+                this.routeTo('tab', query);
+                this.pageInitFn();
+            },
+            routeTo(url, query) {
+                router.push({
+                    path: url,
+                    query: {
+                        status: query.tab_status
+                    }
                 });
-                console.log(this.tableList);
             }
         }
     }
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(Router);
 export default new Router({
     routes: [
         {
-            path: '/',
+            path: '/tab',
             name: 'tab',
             component: Tab
         },
